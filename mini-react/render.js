0266a8a9fae0ce02d03a183bceb1d62a77f08d17
LeeZ1Q <466944567@qq.com>
2023-04-25 20:48:04 +0800

    feat ：Concurrent Mode

--- mini-react/render.js
+++ mini-react/render.js
@@ -10,11 +10,36 @@ function render(element, container) {
     .filter((key) => key !== "children")
     .forEach((key) => {dom[key] = element.props[key]});
 
-  //递归渲染子节点
-  element.props.children.forEach((child) => render(child, dom));
+  // 递归渲染子节点
+  // element.props.children.forEach((child) => render(child, dom));
   
 	//追加到父节点
 	container.appendChild(dom);
+}
+
+let nextUnitOfWork = null;
+
+// 任务调度
+function workLoop(deadline) {
+  // 是否需要让出时间片
+  let shouldYield = false;
+  // 任务执行
+  while (nextUnitOfWork && !shouldYield) {
+    // 执行任务
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    // 判断是否剩余足够时间
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  // 没有足够的时间，请求下一次空闲时间处理
+  requestIdleCallback(workLoop);
+}
+
+// 启动空闲时间处理
+requestIdleCallback(workLoop);
+
+// 任务执行
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO
 }
 
 export default render;
